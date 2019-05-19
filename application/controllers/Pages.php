5c3741122985ff1529f5aac02c96b7c6315f1383
1DreaM1 <36549773+1DreaM1@users.noreply.github.com>
2019-05-19 21:52:29 +0200

    initialisation

---
+++ application/controllers/Pages.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+	public function view($page = 'index') {
+		if(!file_exists(APPPATH.'views/pages/'.$page.'.php')) {
+				show_404();
+		}
+
+		$this->load->view('pages/'.$page);
+	}
+}
